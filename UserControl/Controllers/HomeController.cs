dd43468a89fda6b04f9863e2641225cf057b571d
fedark <fedark@github.com>
2023-03-10 13:21:06 +0300

    Clean up home page.

--- UserControl/Controllers/HomeController.cs
+++ UserControl/Controllers/HomeController.cs
@@ -20,12 +20,7 @@ namespace UserControl.Controllers
 
         public IActionResult Index()
 		{
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
+            return RedirectToAction("Index", "User");
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
