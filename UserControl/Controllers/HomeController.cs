9d766fdca3ffadf7de27396ec573efcb09a3c729
fedark <fedark@github.com>
2023-03-12 20:58:48 +0300

    Code cleanup.

--- UserControl/Controllers/HomeController.cs
+++ UserControl/Controllers/HomeController.cs
@@ -7,13 +7,8 @@ namespace UserControl.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly AppDbContext _context;
-
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+		public HomeController()
         {
-            _logger = logger;
-            _context = context;
         }
 
         public IActionResult Index()
