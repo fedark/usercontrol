dcb807c6d4dc7869c94d4843133d1e6fbcd132b6
fedark <fedark@github.com>
2023-03-12 20:39:15 +0300

    Removed redundant docker script.

--- docker_up.ps1
+++
@@ -1,4 +0,0 @@
-docker stop user
-docker build -t fedark/usercontrol -f Dockerfile .
-docker run -td --rm --name user -p 7887:7887 --link sql fedark/usercontrol
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=UserControl!" -p 1433:1433 -v usercontrol-data:/var/opt/mssql -d --name sql mcr.microsoft.com/mssql/server:2019-latest
